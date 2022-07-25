cfe34b498b6458d9e11f8184abe074d586289d0e
Northern Pines <traviswalborn8@gmail.com>
2022-07-24 21:57:35 -0400

    created html

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// click start begins quiz and timer
+// remove h1 and add h2 "question"
+// add list item buttons for answers
+// when answer is chosen, load next question
+// 
+// list with 4 buttons/answers for each question
+// 
+// wrong answer subtracts time
+//       if user choice === correct answer
+//       then load next questions
+//       else if question is === to other answer
+//       then timer is decremented
+//
+// either all questions answered or timer = 0 ends quiz
+//      timer ends, display message and go back to homepage
+// 
+// at game ends with time, high score and initials can be entered
+//      store remaining time in score variable
+//      place to enter initials
+//      submit button
+//          checks for valid text extry
+// 
+// upon submission, goto highschores page
+//      display top high score(s)     
+// 
+// button to go back to quiz
+//      link to opening page
+// button to clear highscores
